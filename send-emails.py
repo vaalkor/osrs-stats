7acdd79854a7e0f5c19dee0edb526ece3823712f
vaalkor <groggydoggie@protonmail.com>
2021-12-01 16:28:03 +0000

    Sending individual emails to each player

--- send-emails.py
+++ send-emails.py
@@ -31,32 +31,38 @@ with open('players.json', 'r') as read_file:
 players_with_emails = [x for x in players if get_env_var(f'{x}_EMAIL'.replace(' ','_'))]
 print(f'Found emails for players: {players_with_emails}')
 
+if(len(players_with_emails) == 0):
+    raise 'No player emails found!'
+
 with open(args.html_file, 'r') as read_file:
     html = read_file.read()
 
-mailer = emails.NewEmail()
 
-mail_from = {
-    "name": "OSRS Stats Bot",
-    "email": "osts-stats@arbitrarydata.co.uk",
-}
+
 
 recipients = [create_recipient_object(x) for x in players_with_emails]
+for recipient in recipients:
+    print(f'Sending email to {recipient["name"]}')
+    mailer = emails.NewEmail()
 
-reply_to = [
-    {
-        "name": "osrs-stats",
+    mail_from = {
+        "name": "OSRS Stats Bot",
         "email": "osts-stats@arbitrarydata.co.uk",
     }
-]
 
-mail_body = {}
-mailer.set_mail_from(mail_from, mail_body)
-mailer.set_mail_to(recipients, mail_body)
-mailer.set_subject("Daily OSRS stats update!", mail_body)
-mailer.set_html_content(html, mail_body)
-mailer.set_plaintext_content("Sorry, all the juicy info in this email is in the HTML content!", mail_body)
+    reply_to = [
+        {
+            "name": "osrs-stats",
+            "email": "osts-stats@arbitrarydata.co.uk",
+        }
+    ]
 
-# using print() will also return status code and data
-print('Sending email!') 
-print(mailer.send(mail_body))
+    mail_body = {}
+    mailer.set_mail_from(mail_from, mail_body)
+    mailer.set_mail_to([recipient], mail_body)
+    mailer.set_subject("Daily OSRS stats update!", mail_body)
+    mailer.set_html_content(html, mail_body)
+    mailer.set_plaintext_content("Sorry, all the juicy info in this email is in the HTML content!", mail_body)
+
+    # using print() will also return status code and data
+    print(mailer.send(mail_body))
