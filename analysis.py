401ddd10239fb98312ce06eb91d639f6011a4b1f
vaalkor <groggydoggie@protonmail.com>
2021-12-01 02:55:50 +0000

    Got send email script mostly working. Updated analysis python script

--- analysis.py
+++ analysis.py
@@ -3,13 +3,29 @@ import argparse
 import os.path as path
 import os
 import matplotlib.pyplot as plt
+import datetime
 
 parser = argparse.ArgumentParser(description='This script analysis the runescape stats and formulates emails to send out.')
+parser.add_argument('--bucket-url', '-b', dest='bucket_url', required=False, help='S3 bucket url to store images in for graphs in emails. If this is not defined image links will just default to local links.')
 parser.add_argument('--files', '-f', nargs='+', dest='files', required=True, help='1 or more files to parse')
 args = parser.parse_args()
 
 player_stats = {}
 daily_stat_gains = {}
+images_to_upload = []
+timestamp = int(datetime.datetime.utcnow().timestamp())
+
+css=('<style>table, th, td { border: 1px solid black;}'
+'table { margin-left:30px}'
+'</style>'
+)
+
+def create_image_link(image_name):
+    print(image_name)
+    print(args.bucket_url)
+    if(args.bucket_url):
+        return f'{args.bucket_url}/{image_name}'
+    return image_name
 
 
 def compute_daily_xp_gains(player_name, player_data):
@@ -24,7 +40,7 @@ def compute_daily_xp_gains(player_name, player_data):
     for key in current_day['skills'].keys():
         current_day['skills'][key]['previous_xp'] = previous_day['skills'][key]['xp']
         current_day['skills'][key]['daily_xp_gain'] = int(current_day['skills'][key]['xp']) - int(previous_day['skills'][key]['xp'])
-        current_day['skills'][key]['previous_level'] = int(current_day['skills'][key]['level']) - int(previous_day['skills'][key]['level'])
+        current_day['skills'][key]['previous_level'] = int(previous_day['skills'][key]['level'])
     
     daily_xp_gained_skills = [x for x in current_day['skills'].keys() if current_day['skills'][x]['daily_xp_gain'] > 0]
     if len(daily_xp_gained_skills) == 0:
@@ -35,13 +51,14 @@ def compute_daily_xp_gains(player_name, player_data):
     
 
 
-def plot_graph(x_vals, y_vals):
+def plot_graph(x_vals, y_vals, title, filename):
     fig = plt.figure()
     ax=fig.add_subplot(111)
-    # print(x_vals)
-    # print(y_vals)
+    ax.set_title(title)
     ax.bar(x_vals, y_vals)
-    plt.show()
+    plt.xticks(range(len(x_vals)), x_vals, rotation='vertical')
+    plt.tight_layout()
+    plt.savefig(filename)
 
 
 for file in args.files:
@@ -55,48 +72,58 @@ for file in args.files:
     
     player_name = os.path.split(file)[1].replace('.json','')
     compute_daily_xp_gains(player_name, data)
-    # print(daily_stat_gains[player_name])
-    # print(player_stats[player_name])
-    # print([player_stats[player_name]['skills'][x]['daily_xp_gain'] for x in daily_stat_gains[player_name]])
-    plot_graph(daily_stat_gains[player_name], [player_stats[player_name]['skills'][x]['daily_xp_gain'] for x in daily_stat_gains[player_name]])
 
 
-print(json.dumps(daily_stat_gains, indent=2))
-print('----------------------------------------')
+def table_rows(player_name):
+    result = ''
+    for skill in daily_stat_gains[player_name]:
+        print(f'table row for skill: {skill}')
+        result+=\
+('<tr>'
+    f'<td>{skill}</td>'
+    f'<td>{player_stats[player_name]["skills"][skill]["daily_xp_gain"]}</td>'
+    f'<td>{player_stats[player_name]["skills"][skill]["previous_level"]} => {player_stats[player_name]["skills"][skill]["level"]}</td>'
+'</tr>')
+    return result
 
 
-# function ComputePlayerStats($PlayerDataList){
-#     $previousDay = $PlayerDataList | select -last 2 | select -first 1
-#     $currentDay = $PlayerDataList | select -last 1
-
-#     $STATS.skills | %{
-#         $currentDay.skills.$_ | Add-Member -MemberType NoteProperty -Name 'previous_day_xp' -Value $previousDay.skills."$_".xp
-#         $currentDay.skills.$_ | Add-Member -MemberType NoteProperty -Name 'day_delta' -Value ($currentDay.skills."$_".xp - $previousDay.skills."$_".xp)
-#     }
-# }
-# function GetPlayerDailyXpGains($currentDay){
-#     $currentDay.skills `
-#         | Get-Member -MemberType NoteProperty `
-#         | ?{$currentDay.skills.($_.Name).day_delta -ne 0} `
-#         | %{$_.Name} `
-#         | Sort {$currentDay.skills.$_.day_delta}
-# }
-
-# Get-ChildItem "$PSScriptRoot/data" -Filter *.json | %{
-#     $data = Get-Content $_.FullName | ConvertFrom-Json
-#     if(($json | measure | select -ExpandProperty count) -lt 2){
-#         "Skipping $($_.FullName) as it doesn't have 2 or more datapoints in it!"
-#         return
-#     }
-
-#     ComputePlayerStats $data
-
-#     $dailyXpGains = GetPlayerDailyXpGains $data
-#     "==============================================="
-#     "$($_.Name.Replace('.json', '')) daily XP gains:"
-#     $playerStats
-#     "==============================================="
+def create_daily_stats_table(player_name):
+    table_string =\
+('<table>'
+    '<tr>'
+        '<th>Skill</th>'
+        '<th>XP Gain</th>'
+        '<th>Level Gain</th>'
+    '</tr>'
+    f'{table_rows(player_name)}'
+'</table>')
+    return table_string
     
-# }
 
+def daily_stats_email_segment():
+    resultString = '<h2>Daily XP Gains (sorted by highest overall gains)</h2>'
+    for player_name in sorted(daily_stat_gains.keys(), key=lambda x:-1*player_stats[x]['skills']['overall']['daily_xp_gain']):
+        image_name = f'{timestamp}_{player_name}_daily_xp.png'
+        images_to_upload.append(image_name)
 
+        daily_xp_values = [player_stats[player_name]['skills'][x]['daily_xp_gain'] for x in daily_stat_gains[player_name][::-1]]
+        plot_graph(daily_stat_gains[player_name][::-1], daily_xp_values, f'{player_name} XP gained previous day', image_name)
+        
+        table_string = create_daily_stats_table(player_name)
+        resultString += f'<h3>{player_name} daily XP gains</h3><img src="{create_image_link(image_name)}"><img>{table_string}'
+
+    return resultString
+        
+
+# Build email!
+email_string = '<h1> Daily OSRS stats update! </h1><hr/>&nbsp'
+if(len(daily_stat_gains.keys()) > 0):
+    email_string += daily_stats_email_segment()
+
+final_email = f'<html><head>{css}</head><body>{email_string}</body></html>'
+
+with open('email_content.html', 'w') as f:
+    f.write(final_email)
+
+with open('images_to_upload.json', 'w') as f:
+    json.dump(images_to_upload, f)
---
+++ send-email.ps1
@@ -0,0 +1,43 @@
+if(-not $env:S3_BUCKET_URL){ throw 'Could not find requried env var S3_BUCKET_URL' }
+if(-not $env:S3_BUCKET_NAME){ throw 'Could not find requried env var S3_BUCKET_NAME' }
+
+if($env:S3_BUCKET_URL){
+    python analysis.py --bucket-url $env:S3_BUCKET_URL --files (ls data -File -Filter *.json | %{$_.FullName})
+}else{
+    python analysis.py --files (ls data -File -Filter *.json | %{$_.FullName})
+}
+
+if(-not (Test-Path email_content.html)){
+    throw 'Could not find email_content.html file. It should have been produced by analysis.py'
+}
+if(-not (Test-Path images_to_upload.json)){
+    throw 'Could not find email_content.html file. It should have been produced by analysis.py'
+}
+
+aws s3 cp email_content.html s3://$env:S3_BUCKET_NAME/email_content.html
+(Get-Content images_to_upload.json | ConvertFrom-Json) | %{
+    aws s3 cp "`"$_`"" "`"s3://$env:S3_BUCKET_NAME/$_`""
+}
+
+
+# exit 0
+# $destination = @{
+#     ToAddresses = $DestinationAddresses
+#     CcAddresses = @()
+#     BccAddresses = @()
+# } | ConvertTo-Json -Depth 10 -Compress | Set-Content 'temp-destination.json' -Force
+
+# $message = @{
+#     Subject = @{
+#         Data = 'This is a test email lad!'
+#         Charset = 'UTF-8'
+#     }
+#     Body = @{
+#         Html = @{
+#             Data = $Html
+#             Charset = 'UTF-8'
+#         }
+#     }
+# } | ConvertTo-Json -Depth 10 -Compress | Set-Content 'temp-message.json' -Force
+
+# aws ses send-email --from osrs-stats@arbitrarydata.co.uk --destination file://temp-destination.json --message file://temp-message.json
