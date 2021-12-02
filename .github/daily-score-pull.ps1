170c6667e4aba4b03c25fad16a1163d4def66e75
vaalkor <groggydoggie@protonmail.com>
2021-12-02 10:45:26 +0000

    Manual scores update!

--- .github/daily-score-pull.ps1
+++ .github/daily-score-pull.ps1
@@ -1,26 +1,26 @@
-if(-not (Test-Path "$PSScriptRoot/players.json")){
+if(-not (Test-Path "players.json")){
     throw "Could not find players.json file!"
 }
 
-. "$PSScriptRoot/highscores-functions.ps1"
+. "./highscores-functions.ps1"
 
-$players = Get-Content "$PSScriptRoot/players.json" | ConvertFrom-Json
+$players = Get-Content "players.json" | ConvertFrom-Json
 
 $players | %{
     "Pulling scores for player: $_"
     $scores = GetPlayerStats $_
     
-    if(Test-Path -Path "$PSScriptRoot/data/$_.json" -PathType Leaf)
+    if(Test-Path -Path "data/$_.json" -PathType Leaf)
     {
-        "Found existing $PSScriptRoot/data/$_.json file. Appending new scores."
-        $currentScores = Get-Content "$PSScriptRoot/data/$_.json" | ConvertFrom-Json
+        "Found existing data/$_.json file. Appending new scores."
+        $currentScores = Get-Content "data/$_.json" | ConvertFrom-Json
         $currentScores = @($currentScores) + @($scores)
     }
     else
     {
-        "Could not find $PSScriptRoot/data/$_.json file. A new one will be created."
+        "Could not find data/$_.json file. A new one will be created."
         $currentScores = $scores
     }
 
-    $currentScores | ConvertTo-Json -Depth 20 -Compress | Set-Content -Force "$PSScriptRoot/data/$_.json"
+    $currentScores | ConvertTo-Json -Depth 20 -Compress | Set-Content -Force "data/$_.json"
 }
